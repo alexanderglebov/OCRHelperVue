1d2f276b374381e28982b7d8dd3447a26bc42154
roadToDev <o.hlebov@gmail.com>
2018-11-08 17:15:58 +0200

    navi added, buttons added

--- ocr/src/App.vue
+++ ocr/src/App.vue
@@ -1,22 +1,87 @@
-
-
 <template>
-  <b-table dark :items="items"></b-table>
+  <div class="container-fluid">
+  <b-table bordered dark :items="items" :fields="fields"></b-table>
+  </div>
 </template>
 
 <script>
-  const items = [
-    { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-    { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-    { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-    { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-  ]
-
   export default {
     data () {
       return {
-        items: items
+        items: [],
+        fields: []
       }
+    },
+    created: fetchData,
+    updated: fetchData
+  }
+
+  function showHeaders(json) {
+    var headers = ['id', {key: 'oppoName', label: 'Opportunity Name'}]
+    for (var processName in json.processes){
+      headers.push(processName)
+    }
+    return headers
+  }
+
+  function fetchData() {
+    var self = this
+    window.fetch('data.json')
+      .then(function (response) {
+        if (response.status !== 200) {
+          window.alert('Data fetch error, ' + 'status is: ' + response.status + ' ' + response.statusText)
+        }
+        return response.json()
+      })
+      .then(function (myJson) {
+        self.fields = showHeaders(myJson)
+        self.items = getItems(myJson.oppos)
+      })
+  }
+
+  function getItems(oppos) {
+    var items = []
+    oppos.forEach(function (oppo) {
+      var item = {id: oppo.id, oppoName: oppo.oppoName}
+      item = addButtonsToItem(oppo, item)
+     // console.log(item)
+      items.push(item)
+    })
+   // console.log(items)
+    return items
+  }
+
+  function addButtonsToItem(oppo, item) {
+    oppo.processes.forEach(function (process) {
+      var htmlButtons = ''
+      process.stages.forEach(function (stage) {
+        htmlButtons += addButton(stage) + ' '
+      })
+      item[process.processName] = htmlButtons
+    })
+    return item
+  }
+
+  function addButton(stage) {
+    switch (stage.stageStatus.statusStr) {
+      case 'CompletedStStatus':
+        return '<input type="button" value="' + stage.stageNameStr + '" class="btn btn-success2 btn-sm">'
+        break
+      case 'ErrorStStatus':
+        return '<input type="button" value="' + stage.stageNameStr + '" class="btn btn-danger1 btn-sm">'
+        break
+      case 'InProcessStStatus':
+        return '<input type="button" value="' + stage.stageNameStr + '" class="btn btn-warning btn-sm">'
+        break
+      case 'NotStartedStStatus':
+        return '<input type="button" value="' + stage.stageNameStr + '" class="btn btn-secondary btn-sm">'
+        break
+      case 'StoppedStStatus':
+        return '<input type="button" value="' + stage.stageNameStr + '" class="btn btn-info btn-sm">'
+        break
+      default:
+        break
     }
   }
+
 </script>
